849955185736a1d266a25d01a5278dc4c1671da2
Bibek <137331749+immbibek@users.noreply.github.com>
2024-11-15 15:34:37 +0530

    Createing Header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,74 @@
+'use client';
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import Link from 'next/link';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { usePathname } from 'next/navigation'; // Importing the usePathname hook
+import { useState } from 'react';
+
+export default function Header() {
+  const path = usePathname(); // Using usePathname hook to get current path
+  
+  const[theme,setTheme]=useState(false)
+
+  return (
+    <Navbar className='border-b-2'>
+      <Link
+        href='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sahand&apos;s
+        </span>
+        Blog
+      </Link>
+
+      <form>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        <Link href='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue' outline>
+            Sign In
+          </Button>
+        </Link>
+
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Link href='/'>
+          <Navbar.Link active={path === '/'} as={'div'}>
+            Home
+          </Navbar.Link>
+        </Link>
+        <Link href='/about'>
+          <Navbar.Link active={path === '/about'} as={'div'}>
+            About
+          </Navbar.Link>
+        </Link>
+        <Link href='/projects'>
+          <Navbar.Link active={path === '/projects'} as={'div'}>
+            Projects
+          </Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
